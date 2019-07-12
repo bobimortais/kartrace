aa94cceeac12c5c5458cec47f5d1111185de33fa
bobimortais <robsonzacarias@hotmail.com>
2019-07-11 22:16:21 -0300

    Update test files

--- src/test/java/TestRaceProcessing.java
+++ src/test/java/TestRaceProcessing.java
@@ -1,35 +1,43 @@
+import com.test.kartrace.util.FileProcessor;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TestRaceProcessing
 {
     @Test
     public void TEST_FILE_LOAD_SUCESS()
     {
-        Assert.assertTrue(true);
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
+        Assert.assertTrue(fileLines.size() > 0);
     }
 
     @Test
     public void TEST_FILE_LOAD_FAIL()
     {
-        Assert.assertTrue(true);
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_3.txt");
+        Assert.assertTrue(fileLines.size() == 0);
     }
 
     @Test
     public void POSITION_CORRECT()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 
     @Test
     public void CHECK_COMPLETED_LAPS()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 
     @Test
     public void CHECK_TOTAL_RACE_TIME()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 
@@ -42,24 +50,28 @@ public class TestRaceProcessing
     @Test
     public void CHECK_RACE_BEST_LAP()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 
     @Test
     public void CHECK_GAP_TO_LEADER()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 
     @Test
     public void CHECK_DRIVER_RACE_AVG_SPEED()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 
     @Test
     public void CHECK_ALL_DATA_SHOWN()
     {
+        List<String> fileLines = FileProcessor.getLinesFromFile("src/test/resources/kartlog_1.txt");
         Assert.assertTrue(true);
     }
 }
